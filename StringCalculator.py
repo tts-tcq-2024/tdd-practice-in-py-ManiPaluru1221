a06276ddf9adbc4491ffcb06097f6a3ab54739e7
ManiPaluru1221 <palurumani1999@gmail.com>
2024-08-03 23:01:06 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,1 +1,17 @@
-
+def add(input):
+  delim = ','
+  sum = 0
+  if(input[0] == '/'):
+    ind = input.find('\n')
+    delim = input[2:ind+1]
+    input = input[ind+1:]
+    if delim.length() > 0:
+      delim.erase(0,1)
+      delim.erase(delim.length()-1,1)
+  numbers = input.split([delim, '\n'])
+  
+  for item in numbers:
+    if int(item) >= 1000:
+      continue
+    sum += item
+  return sum
